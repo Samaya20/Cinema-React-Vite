18915c4989480e6d9732f4c3d4ed58946a08cd2b
Samaya Mughumova <81756158+Samaya20@users.noreply.github.com>
2024-07-12 16:10:54 +0400

    Update fetch.js

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,7 +1,7 @@
 export const fetchCinemas = async () => {
   try {
     const response = await fetch(
-      "https://raw.githubusercontent.com/Samaya20/Fake-Films-API/main/films.json"
+      "https://raw.githubusercontent.com/Samaya20/Fake-Datas/master/films.json"
     );
     const data = await response.json();
     return data.cinemas;
